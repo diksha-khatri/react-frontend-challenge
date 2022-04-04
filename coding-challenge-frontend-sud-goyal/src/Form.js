cdbe37360ea356456285e1d6aa27a39d11941d79
diksha-khatri <dikshakhatri2626@gmail.com>
2022-04-04 22:11:33 +0530

    3rd commit

--- coding-challenge-frontend-sud-goyal/src/Form.js
+++ coding-challenge-frontend-sud-goyal/src/Form.js
@@ -1,146 +1,183 @@
 import React from "react";
-import axios from "axios"
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
+import Axios from "axios"
 // import AsyncSelect from "react-select/async"
 // import Select from "react-select";
 // import {GoogleMap , withScriptjs, withGoogleMap } from "google-maps-react"
 // import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
- 
-export default function Form(){
-const options = []  
-    
-        const [data , setdata] = useState([])
-                         
-        
 
-        useEffect(()=>{
-                const getdata = async ()=>{
-                const res = await fetch("https://jsonplaceholder.typicode.com/users")
-                const getinfo = await res.json()
-                console.log(getinfo)
-                setdata( await getinfo)
-            }
-            getdata()
-        },[])
+export default function Form() {
 
-    
+
+    const [data, setdata] = useState([])
+    const [userid, setuserid] = useState("")
+    const [post, setpost] = useState([])
+    // s
+    const Key = "AIzaSyD-sAqxa1pZqF4tk7eR83ZS7wOCEmsxKwI"
+    let Lat
+    let Lng
+    let url
+
+    useEffect(() => {
+        const getdata = async () => {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users")
+            const getinfo = await res.json()
+            console.log(getinfo)
+            setdata(await getinfo)
+        }
+        getdata()
+    }, [])
+
+    // const handleData = (event) => {
+    //     const getuserid = event.target.value
+    //     setuserid(userid)
+    // }
+    // useEffect(() => {const options = {
+    //     method: "POST",
+    //     body: JSON.stringify(data),
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // }
+    //     const getpost = async () => {
+    //         const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${userid}`,{method: "POST",
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }})
+    //         const getpost = await res.json()
+    //         console.log(getpost)
+    //         setpost(await getpost)
+    //     }
+    //     getpost()
+    // }, [userid])
     const [formData, setFormData] = React.useState(
         {
-            title:"",
-            body:"",
-            user:"",
-            id:""
+            title: "",
+            body: "",
+            user: "",
+            id: "",
+            latlng: ""
+
         }
     )
+    // const [postData, setpostData] = React.useState(
+    //     {
+    //         title: "",
+    //         body: "",
+    //         id: "",
+
+    //     }
+    // )
     console.log(formData)
-    function handleChange(event){
-        
-        const {name,value,type,checked} = event.target
+    function handleChange(event) {
+
+        const { name, value, type, checked, id } = event.target
         setFormData(prevFormData => {
-            return{
+            return {
                 ...prevFormData,
-                [name]: type === "checked" ? checked : value
+                [name]: type === "checked" ? checked : value,
+
             }
-        })    
-    }   
-    function handleSubmit(event){
-        
-        event.preventDefault()
+        })
     }
-   return( 
-       <div className="formContainer">
-            <form className="form" onSubmit={handleSubmit}>
-                <label htmlFor="user">What is your favorite color?</label>
-                    <select className="form--input" id="user" 
-                    value={formData.id}
-                    onChange={handleChange}
-                    name="id"
-                    >  
-                        
-                        <option value="">--Choose--</option>
-                    
-                        {
-                            data.map( (item) => (
-                            <option key={item.id} value={item.id}>{item.name}</option>
-                            ))
-                        }
-                        
-                        
-                        
-                    </select>
-                        
-                            
-                                {/* <iframe
-                                width="300"
-                                height="250"
-                                style="border:0"
-                                loading="lazy"
-                                allowfullscreen
-                                referrerpolicy="no-referrer-when-downgrade"
-                                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD-sAqxa1pZqF4tk7eR83ZS7wOCEmsxKwI
-                                    &q={{lat=item.geo.lat},{lng=item.geo.lng}},Seattle+WA">
-                                </iframe>
-                 */}
-                        
-                        
-                    
-                    {/* <Map google={this.props.google} zoom={14}>
- 
-                    <Marker onClick={this.onMarkerClick}
-                     name={'Current location'} />
+    // function handlepost(e){
+    //     setpostData () => {
+    //     return {
+    //         const newdata = { ...post }
+    //         newdata[e.target.id] = e.target.value,
+    //         setpost(newdata),
+    //         console.log(setpost)
+    //     }}
+    // }
 
-                       <InfoWindow onClose={this.onInfoWindowClose}>
-                    <div>
-                    <h1>{this.state.selectedPlace.name}</h1>
-                    </div>
-                    </InfoWindow>
-                    </Map> */}
+function handleSubmit(event) {
 
-                    <br /> 
-                    {/* <img src="./images/earth.png" width="100px"></img> */}
-
-                    <textarea className="form--input" 
-                        placeholder="title"
-                        name="title"
-                        value={formData.title}
-                        onChange={handleChange}
-                    />
-                    <textarea  className="form--input"
-                        placeholder="body"
-                        name="body"
-                        value={formData.body}
-                        onChange={handleChange}
-                    />
-                    <button className="form--submit" type="submit">Submit</button>
-            </form>
-       </div>
-   )
+    event.preventDefault()
+    // Axios.post(url, {
+    //     id: postData.id,
+    //     title: postData.title,
+    //     body: postData.body
+    // })
+    //     .then(res => {
+    //         console.log(res.postData)
+    //     })
 }
-// function map(){
-//     return(
-//         <Map
-//         google={this.props.google}
-//         style={style}
-//         initialCenter={{
-//           lat: [this.item.geo.lat],
-//           lng: [this.item.geo.lng]
-//         }}
-//         zoom={15}
-//         onClick={this.onMapClicked}
-//       />
-//     )
-// }
-// const WrappedMap = withScriptjs(withGoogleMap(map))
+return (
+    <div className="formContainer">
+        <form className="form" onSubmit={handleSubmit}>
+            <label htmlFor="user">Select a user</label>
+            <select className="form--input" id="user"
+                value={formData.id}
+                onChange={handleChange}
+                name="id"
 
-// function GoogleApiWrapper(){
-//     return(
-//         <WrappedMap
-//             googleMapURl = {"AIzaSyD-sAqxa1pZqF4tk7eR83ZS7wOCEmsxKwI"}
-//             loadingElement={<div style={{height = "100%"}} />}
-//             containerElement={<div style={{height = "100%"}} />}
-//             mapElement={<div style={{height = "100%"}} />}
-//          />
-//     )
-//      }
+            >
 
-//  export {GoogleApiWrapper} 
+                <option value="">--Choose--</option>
+
+                {
+                    data.map((item) => (
+                        <option key={item.id} value={item.id}>{item.name}</option>
+                        // <Map
+                        //     google={this.props.google}
+                        //     // style={style}
+                        //     initialCenter={{
+                        //     lat: [this.item.address.geo.lat],
+                        //     lng: [this.item.address.geo.lng]
+                        //     }}
+                        //     zoom={15}
+                        //     onClick={this.onMapClicked}
+                        // />
+                        //     <img name="latlng"
+                        //     value={formData.latlng}
+                        //     onchange={handleChange}
+                        //     src = {`https://maps.googleapis.com/maps/api/geocode/json?latlng=${item.address.geo.lat},${item.address.geo.lng}&key=${Key}`}
+                        //    /> 
+                    ))
+                }
+
+                {/* {
+                                    data.map((item)=>(
+                                        <img 
+                                        src = {`https://maps.googleapis.com/maps/api/geocode/json?latlng=${item.address.geo.lat},${item.address.geo.lng}&key=${Key}`}
+                                        />
+                                        ))
+                                }  */}
+
+                {/* console.log(Lat,Lng) */}
+
+
+
+            </select>
+
+            {/* {
+                data.map((item) => (
+                    Credential = "sameorigin",
+                    <img
+                        src={`https://maps.googleapis.com/maps/api/geocode/json?latlng=${item.address.geo.lat},${item.address.geo.lng}&key=${Key}`}
+                    />
+
+                ))
+
+            } */}
+        
+                <textarea className="form--input"
+                    placeholder="title"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                />,
+                <textarea className="form--input"
+                    placeholder="body"
+                    name="body"
+                    value={formData.body}
+                    onChange={handleChange}
+                />
+        
+        
+            <button className="form--submit" type="submit">Submit</button>
+        </form>
+    </div>
+)
+    }
